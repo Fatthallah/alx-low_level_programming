dc9a16cbbf4cad678a65f708111d5424e77bbd3f
Fatthallah <eng.fatthallah@gmail.com>
2023-08-29 10:14:24 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,12 @@
  * Return: What it returns is #Number_of_Nodes.
  */
 
-size_t print_listint(const listint_t *p){
+size_t print_listint(const listint_t *h){
 	size_t num = 0;
-	while(p){
-		printf("%d\n", p->n);
+	while(h){
+		printf("%d\n", h->n);
 		num++;
-		p = p->next;
+		h=h->next;
 	}
 	return(num);
 }

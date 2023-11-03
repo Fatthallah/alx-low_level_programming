7448d91fa2321d45d3486686a15bfa79c940ecf5
Fatthallah <eng.fatthallah@gmail.com>
2023-11-03 01:43:10 -0700

    0x1A. C - Hash tables

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - fun
+ * @key: ref
+ * @size: ref
+ * Return: ref
+ * Description: ref
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
+
